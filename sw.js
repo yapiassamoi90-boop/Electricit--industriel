522c5632bbe9e4c8bb9c6599f3a25284773251c9
yapiassamoi90-boop <yapiassamoi90@gmail.com>
2026-09-10 06:17:48 +0000

    Update service worker for caching and activation

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
-const CACHE_NAME = 'elec-industrielle-v1';
-const urlsToCache = ['index.html', 'style.css', 'script.js', 'manifest.json'];
+const CACHE_NAME = 'elec-industrielle-v2';
+const urlsToCache = ['./','./index.html','./style.css','./script.js','./manifest.json','./icon-192.png','./icon-512.png'];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener('activate', e => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html'))));
 });
